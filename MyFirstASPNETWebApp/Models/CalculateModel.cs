6a6bb7e5c0af68da0e9ede3d2b2e4ee8af4d9f9b
Chad Yorgason <chad.yorgason@gmail.com>
2023-02-06 22:52:56 -0700

    all

---
+++ MyFirstASPNETWebApp/Models/CalculateModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Models
+{
+    public class CalculateModel
+    {
+        [Range(0,100)]
+        public string pctA { get; set; }
+        [Range(0,100)]
+        public string pctGP { get; set; }
+        [Range(0, 100)]
+        public string pctQ { get; set; }
+        [Range(0, 100)]
+        public string pctME { get; set; }
+        [Range(0, 100)]
+        public string pctFE { get; set; }
+        [Range(0, 100)]
+        public string pctI { get; set; }
+
+    }
+}
